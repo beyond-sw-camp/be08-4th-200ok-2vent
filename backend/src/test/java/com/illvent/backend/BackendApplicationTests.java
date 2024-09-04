57028ca39ddfb85c1010dcd24ae9a59b84fbe773
henhen7 <eatho@naver.com>
2024-09-05 03:33:02 +0900

    unknown test

--- backend/src/test/java/com/illvent/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/illvent/backend/BackendApplicationTests.java
@@ -3,11 +3,11 @@ package com.illvent.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
