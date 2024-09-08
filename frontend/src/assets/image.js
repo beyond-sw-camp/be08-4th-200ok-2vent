0f29a991cb25ec3436fbe5b6793e1abf3c018cee
박성준 <macleod.park@gmail.com>
2024-09-09 03:53:26 +0900

    front test

--- frontend/src/assets/image.js
+++ frontend/src/assets/image.js
@@ -1,43 +1,21 @@
-// export default [{
-//     id : 0,
-//     image: "../assets/images/001.png",
-//     },
-//     {
-//     id : 1,
-//     image: "../assets/images/003.png",
-//     },
-//     {
-//     id : 2,
-//     image: "../assets/images/004.png",
-//     },
-//     {
-//     id : 3,
-//     image: "../assets/images/005.png",
-//     },
-//     {
-//     id : 4,
-//     image: "../assets/images/006.png",
-//     }
-// ];
-
 export default [{
     id : 0,
-    image: `/Users/sjpark/Desktop/DevOpsProject/frontend/public/images/001.png`,
+    image: "../assets/images/001.png",
     },
     {
     id : 1,
-    image: `/Users/sjpark/Desktop/DevOpsProject/frontend/public/images/003.png`,
+    image: "../assets/images/003.png",
     },
     {
     id : 2,
-    image: `/Users/sjpark/Desktop/DevOpsProject/frontend/public/images/004.png`,
+    image: "../assets/images/004.png",
     },
     {
     id : 3,
-    image: `/Users/sjpark/Desktop/DevOpsProject/frontend/public/images/005.png`,
+    image: "../assets/images/005.png",
     },
     {
     id : 4,
-    image: `/Users/sjpark/Desktop/DevOpsProject/frontend/public/images/006.png`,
+    image: "../assets/images/006.png",
     }
 ];
