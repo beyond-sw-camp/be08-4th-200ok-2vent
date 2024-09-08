ba9e80b8ca77510e036379223e0a16abbfb1141a
박성준 <macleod.park@gmail.com>
2024-09-09 03:06:58 +0900

    front test

--- frontend/src/assets/image.js
+++ frontend/src/assets/image.js
@@ -1,21 +1,43 @@
+// export default [{
+//     id : 0,
+//     image: "../assets/images/001.png",
+//     },
+//     {
+//     id : 1,
+//     image: "../assets/images/003.png",
+//     },
+//     {
+//     id : 2,
+//     image: "../assets/images/004.png",
+//     },
+//     {
+//     id : 3,
+//     image: "../assets/images/005.png",
+//     },
+//     {
+//     id : 4,
+//     image: "../assets/images/006.png",
+//     }
+// ];
+
 export default [{
     id : 0,
-    image: "../assets/images/001.png",
+    image: `${process.env.PUBLIC_URL}/images/001.png`,
     },
     {
     id : 1,
-    image: "../assets/images/003.png",
+    image: `${process.env.PUBLIC_URL}/images/003.png`,
     },
     {
     id : 2,
-    image: "../assets/images/004.png",
+    image: `${process.env.PUBLIC_URL}/images/004.png`,
     },
     {
     id : 3,
-    image: "../assets/images/005.png",
+    image: `${process.env.PUBLIC_URL}/images/005.png`,
     },
     {
     id : 4,
-    image: "../assets/images/006.png",
+    image: `${process.env.PUBLIC_URL}/images/006.png`,
     }
 ];
